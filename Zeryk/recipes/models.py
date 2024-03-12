bd1c671ddf60e3e4ffadc591aa5715ae92f17956
Masa <mariia.gavrylenko@student.gyarab.cz>
2024-03-12 23:39:11 +0100

    dodelat vsechno prosim

---
+++ Zeryk/recipes/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
+
+#TODO dodelat pridat ingridients, pole, vytvareni pole, databaze, nejak spojit
+
+
+class Recipe(models.Model):
+  title = models.CharField(max_length=100)
+  description = models.TextField()
+
+  author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+
+  def get_absolute_url(self):
+      return reverse("recipes-detail", kwargs={"pk": self.pk})
+
+  def __str__(self):
+    return self.title
