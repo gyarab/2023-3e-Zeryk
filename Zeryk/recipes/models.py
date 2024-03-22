5196b40ea49fb24efa1756c52db9685ead2c92d1
maruška <93185278+maruska7@users.noreply.github.com>
2024-03-22 11:21:27 +0100

    vsechno dodelany

--- Zeryk/recipes/models.py
+++ Zeryk/recipes/models.py
@@ -32,7 +32,7 @@ class Recipe(models.Model):
 class Comment(models.Model):
   post = models.ForeignKey(Recipe, related_name="comments", on_delete=models.CASCADE)
   name = models.CharField(max_length=255)
-  body = models.TextField()
+  body = RichTextField(blank=True, null=True)
   date_added = models.DateTimeField(auto_now_add=True)
 
   def __str__(self):
