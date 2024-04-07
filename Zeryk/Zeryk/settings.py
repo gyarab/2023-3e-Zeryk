70fe37afe99ae18e4e588827b1eceb30851361fb
prach <matej.kratochvil@student.gyarab.cz>
2024-04-07 15:37:15 +0200

    miss

--- Zeryk/Zeryk/settings.py
+++ Zeryk/Zeryk/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6bbhxfn&d5jl5xtr3tqqe4_oh*osk^1bx98od$+r6*y3-##m7z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
